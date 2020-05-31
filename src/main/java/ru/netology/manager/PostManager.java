07696c0a5720513074799baeab48c318782e1df6
Pavel Smolkov <smolcov@bk.ru>
2020-06-01 00:20:50 +0300

    task 2 (PostManager)

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,24 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostInfo;
+
+public class PostManager {
+    private PostInfo[] postInfos;
+
+    private int ownerId;
+    private String domain;
+    private String query;
+    private boolean ownernly;
+    private int count;
+    private int offset;
+    private int postId;
+
+    public PostInfo[] search() {
+
+        return null;
+    }
+
+    public void delete() {
+
+    }
+}
